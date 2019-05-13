c6b3d8fb15bc4fcfcfd8cb9d0325ec9c46404a9a
ashinpchandran <ashin.ashin007@gmail.com>
2019-05-13 12:43:53 +0530

    Splash Screen

---
+++ app/src/main/java/com/tapp/dayfinder/MainActivity.java
@@ -0,0 +1,55 @@
+package com.tapp.dayfinder;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_main);
+        Thread logotimer = new Thread(){
+            public void run(){
+
+                try
+                {
+                    sleep(4000);
+                    Intent HomeIntent = new Intent("android.intent.action.HOME");
+                    startActivity(HomeIntent);
+                }
+                catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                finally
+                {
+                    finish();
+                }
+            }
+        };
+        logotimer.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+
+    }
+
+
+
+}
