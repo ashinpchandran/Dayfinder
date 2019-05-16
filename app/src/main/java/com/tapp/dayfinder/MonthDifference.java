f7fe35a7355a23d0e78e6ae31f46abe6786e74dd
ashinpchandran <ashin.ashin007@gmail.com>
2019-05-16 11:32:18 +0530

    feat(button_color): Button Color of MonthDifference

--- app/src/main/java/com/tapp/dayfinder/MonthDifference.java
+++ app/src/main/java/com/tapp/dayfinder/MonthDifference.java
@@ -23,7 +23,6 @@ public class MonthDifference extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Making notification bar transparent
-
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             assert getSupportActionBar() != null;
             getSupportActionBar().hide();
